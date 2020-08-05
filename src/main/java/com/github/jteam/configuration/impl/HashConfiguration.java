3c03adad31f95a8c29ffa40ee0e6509ebb7b5146
wangpeng <hongshuboy@qq.com>
2020-08-05 12:51:52 +0800

    重构configuration

--- src/main/java/com/github/jteam/configuration/impl/HashConfiguration.java
+++ src/main/java/com/github/jteam/configuration/impl/HashConfiguration.java
@@ -1,19 +1,22 @@
-package com.github.jteam.comfiguration;
+package com.github.jteam.configuration.impl;
 
+import com.github.jteam.configuration.Configuration;
 import com.github.jteam.value.Type;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
- * 配置类，用于配置框架为各类型赋值时的默认值，若对默认值没有特殊要求，请使用<code>SINGLE_CONFIGURATION</code><br/>
- * 若有特殊要求，如Integer为null时，希望框架自动赋值为1（默认为0），可以使用 {@link #setDefaultConfig(Type, Object)}
- *
- * @author hongshuboy
- * 2020-08-03 12:55
+ * @author wp
+ * 2020-08-05 12:37
  */
-public class Configuration {
-    public static final Configuration SINGLE_CONFIGURATION = new Configuration(false);
-    private Boolean modify;//是否可以被修改
+public class HashConfiguration implements Configuration {
+    public static final Configuration SINGLE_CONFIGURATION = new HashConfiguration(false);
+    private final boolean modify;//是否可以被修改
+    private static final  String IGNORE_FIELD = "ignoreField";
     private final HashMap<String, Object> configMap = new HashMap<>();
 
     {
@@ -36,6 +39,7 @@ public class Configuration {
         configMap.put(Type.LONG_ARRAY.getType(), new Long[0]);
         configMap.put(Type.DOUBLE_ARRAY.getType(), new Double[0]);
         configMap.put(Type.STRING_ARRAY.getType(), new String[0]);
+        configMap.put(IGNORE_FIELD, new HashSet<String>());
     }
 
     /**
@@ -44,6 +48,7 @@ public class Configuration {
      * @param type  使用Type枚举类设置，如Type.Integer
      * @param value 为Type设置的默认值
      */
+    @Override
     public Configuration setDefaultConfig(Type type, Object value) {
         modifiable();
         configMap.put(type.getType(), value);
@@ -56,6 +61,7 @@ public class Configuration {
      * @param type 需要查看的属性，如Type.Integer
      * @return 为该属性设置的默认值
      */
+    @Override
     public Object getDefaultValue(Type type) {
         return configMap.get(type.getType());
     }
@@ -66,6 +72,7 @@ public class Configuration {
      * @param type 需要查看的属性
      * @return 为该属性设置的默认值
      */
+    @Override
     public Object getDefaultValue(String type) {
         return configMap.get(type);
     }
@@ -76,11 +83,33 @@ public class Configuration {
         }
     }
 
-    public Configuration() {
+    public HashConfiguration() {
         this.modify = true;
     }
 
-    public Configuration(Boolean modify) {
+    public HashConfiguration(Boolean modify) {
         this.modify = modify;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Set<String> getIgnoreFields() {
+        return (Set<String>) configMap.get(IGNORE_FIELD);
+    }
+
+    /**
+     * 设置需要跳过的字段名（将自动忽略大小写）
+     *
+     * @param fields 如name，则将不对name字段赋默认值
+     * @return Configuration
+     */
+    @SuppressWarnings({"unchecked"})
+    @Override
+    public Configuration setIgnoreFields(String... fields) {
+        modifiable();
+        Set<String> ignoreKeys = (Set<String>) configMap.get(IGNORE_FIELD);
+        ignoreKeys.addAll(Arrays.asList(fields));
+        configMap.put(IGNORE_FIELD, ignoreKeys.stream().map(String::toLowerCase).collect(Collectors.toSet()));
+        return this;
     }
 }
