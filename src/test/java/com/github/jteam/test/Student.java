791a634aa5dbeab369958f2154db0902e327f3ec
wangpeng <hongshuboy@qq.com>
2020-08-05 17:42:35 +0800

    完善测试指导

--- src/test/java/com/github/jteam/test/Student.java
+++ src/test/java/com/github/jteam/test/Student.java
@@ -1,19 +1,78 @@
 package com.github.jteam.test;
 
-import lombok.*;
+import java.util.Arrays;
 
 /**
+ * 测试类，注意num字段是基本类型，本身就有默认值，本工具不会对基本类型做任何操作
  * @author wp
  * 2020-08-03 14:25
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class Student {
     private Integer age;
     private String name;
     private Integer[] ids;
+    private String schoolName;
     private int num;
 
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer[] getIds() {
+        return ids;
+    }
+
+    public void setIds(Integer[] ids) {
+        this.ids = ids;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public Student() {
+    }
+
+    public Student(Integer age, String name, Integer[] ids, String schoolName, int num) {
+        this.age = age;
+        this.name = name;
+        this.ids = ids;
+        this.schoolName = schoolName;
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", ids=" + Arrays.toString(ids) +
+                ", schoolName='" + schoolName + '\'' +
+                ", num=" + num +
+                '}';
+    }
 }
--- src/test/java/com/github/jteam/test/Test1.java
+++ src/test/java/com/github/jteam/test/Test1.java
@@ -1,7 +1,8 @@
 package com.github.jteam.test;
 
-import com.github.jteam.comfiguration.Configuration;
 import com.github.jteam.JHelper;
+import com.github.jteam.configuration.Configuration;
+import com.github.jteam.configuration.impl.HashConfiguration;
 import com.github.jteam.value.Type;
 import org.junit.Test;
 
@@ -11,23 +12,59 @@ import org.junit.Test;
  */
 public class Test1 {
 
+    /**
+     * 默认赋值方法
+     * 最简单的使用方式，一行代码即可让对象的属性获得初始化的默认值
+     */
     @Test
     public void test1() {
         final Student student = new Student();
 
-        JHelper.setDefaultValue(student, Configuration.SINGLE_CONFIGURATION);
+//      自动赋值
+        JHelper.setDefaultValue(student);
 
         System.out.println(student);
     }
 
+    /**
+     * 自定义默认值
+     * 假如我们想对Integer类型设置的默认值是1，那么可以单独进行设置
+     */
     @Test
     public void test2() {
         final Student student = new Student();
 
-        final Configuration configuration = new Configuration();
+        final Configuration configuration = new HashConfiguration();
         configuration.setDefaultConfig(Type.INTEGER, 1);
         configuration.setDefaultConfig(Type.STRING, "hi");
-        configuration.setDefaultConfig(Type.INTEGER_ARRAY, new Integer[]{1});
+        configuration.setDefaultConfig(Type.INTEGER_ARRAY, new Integer[]{1, 2});
+
+        JHelper.setDefaultValue(student, configuration);
+
+        System.out.println(student);
+    }
+
+    /**
+     * 设置忽略字段
+     * 假如我们想对某些字段跳过，不让工具自动赋默认值，比如{XxxEntity}实体类，因为保存到数据库id自增的关系
+     * 我们希望id字段能保持为null，不要自动赋值，就可以使用{@link Configuration#setIgnoreFields(String...)}
+     *
+     * 1.设置INTEGER类型赋值为1（默认框架赋值为0）
+     * 2.自动赋值时，忽略ids和schoolNamE两个字段（可写1至多个字段，自动会忽略大小写进行匹配）
+     */
+    @Test
+    public void test3() {
+        final Student student = new Student();
+
+        final Configuration configuration = new HashConfiguration();
+        configuration.setDefaultConfig(Type.INTEGER, 1);
+        configuration.setDefaultConfig(Type.STRING, "hi");
+        configuration.setDefaultConfig(Type.INTEGER_ARRAY, new Integer[]{1, 2});
+        /**
+         *  这里虽然设置的是ids和schOOlNamE字段，但实际比较的是ids和schoolName（会自动忽略大小写）
+         *  同时，类中的字段在匹配时，也会被忽略大小写，所以这里可以完全不管大小写，均可匹配到属性
+         */
+        configuration.setIgnoreFields("ids", "schOOlNamE");
 
         JHelper.setDefaultValue(student, configuration);
 
